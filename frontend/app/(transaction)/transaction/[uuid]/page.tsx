935b6351ff13e42e2fe99ee557220ff582d6a3bc
takashi-kurino <takashikurino6@gmail.com>
2026-04-08 18:42:39 +0900

    item一覧の追加

--- frontend/app/(transaction)/transaction/[uuid]/page.tsx
+++ frontend/app/(transaction)/transaction/[uuid]/page.tsx
@@ -1,23 +1,39 @@
 
 import {TransactionDetail} from "../endpoints";
-import DeleteTransaction from "../components/DeleteTransactioin";
+import DeleteTransaction from "../components/DeleteTransaction";
 import EditTransactionForm from "../components/EditTransaction";
+
+
+type Item = {
+  uuid: string;
+  name: string;
+  amount: string;
+  price: number;
+};
 
 export default async function Page({ params }: { params: Promise<{ uuid: string }> }) {
 
   const { uuid } = await params;
   const transaction = await TransactionDetail(uuid);
+  const item : Item[] = await transaction.items;
 
   return (
     
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Transaction Page</h1>
-      {/* <div className="bg-gray-100 p-4 rounded-lg"> */}
-
-        {/* <pre>{JSON.stringify(transaction, null, 2)}</pre> */}
-      {/* </div> */}
 
       <EditTransactionForm uuid={uuid} transaction={transaction} />
+      
+        <ul>
+          {item.map((item) => (
+            
+            <li key={item.uuid} className="mb-2">
+              <strong>name:</strong> {item.name} 
+              <strong>amount:</strong> {item.amount}
+              <strong>price:</strong> {item.price}
+            </li>
+          ))}
+        </ul>
       <DeleteTransaction uuid={uuid} />
 
     </div>
