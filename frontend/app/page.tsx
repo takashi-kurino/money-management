905aff38bccf8a5aa3a2f7e562d7ab314d257d1c
takashi-kurino <takashikurino6@gmail.com>
2026-06-07 21:44:59 +0900

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -14,18 +14,18 @@ export default function Home() {
           This portforil include money management and auth.
         </h2>
         
-        <h2 className="text-xl text-gray-600 mb-2 p-3">
+        <h3 className="text-xl text-gray-600 mb-2 p-3">
           Link To my Profile
-        </h2>
+        </h3>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
           <CardHome title="GitHub" description="github" link="https://github.com/takashi-kurino/money-management"/>
           <CardHome title="Notion" description="write detail this portfolio" link="https://www.notion.so/Takashi-Kurino-Portfolio-356bb43e5cae8015900cc17d23b639bb?source=copy_link" />
         </div>
         
-        <h2 className="text-xl text-gray-600 mb-2 p-3">
+        <h3 className="text-xl text-gray-600 mb-2 p-3">
           Deploy Environment
-        </h2>
+        </h3>
 
         <div className="grid grid-cols-1 sm:grid-cols-2  gap-6">
           <CardHome title="Frontend" description="Vercel" link="https://vercel.com/"/>
@@ -34,20 +34,21 @@ export default function Home() {
           <CardHome title="Email" description="Resend" link="https://resend.com/"/>
         </div>
 
-        <h2 className="text-xl text-gray-600 mb-2 p-3">
+        <h3 className="text-xl text-gray-600 mb-2 p-3">
           Use Framework
-        </h2>
+        </h3>
 
         <div className="grid grid-cols-1 sm:grid-cols-2  gap-6">
           <CardHome title="Frontend" description="React + Next.js" />
           <CardHome title="Backend" description="Django" />
         </div>
 
-        <h2 className="text-xl text-gray-600 mb-2 p-3">
+        <h3 className="text-xl text-gray-600 mb-2 p-3">
           Local Dev Environment
-        </h2>
+        </h3>
 
         <div className="grid grid-cols-1 sm:grid-cols-2  gap-6">
+          <CardHome title="Container" description="Docker" />
           <CardHome title="Database" description="PostgreSQL" />
         </div>
 
